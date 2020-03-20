684680caa373c884ad7008c9deeceb66c5874132
Şafak Bilici <44443472+safakkbilici@users.noreply.github.com>
2020-03-20 18:37:14 +0300

    Update CLT.py

--- Central Limit Theorem with Rolling a Dice/CLT.py
+++ Central Limit Theorem with Rolling a Dice/CLT.py
@@ -37,7 +37,7 @@ class Dice(object):
                 rolled_dice[i,rolled_dice.shape[1] - 1] = sum_of/(rolled_dice.shape[1] - 1)
         return rolled_dice
         
-    def PDF(self,y):
+    def getPDF(self,y):
         fig = plt.figure(figsize=(10,10))
         sns.distplot(y)
         plt.xlabel('X')
@@ -59,5 +59,5 @@ if __name__ == "__main__":
 
    y = rolled_dice[:,N_ROLL]
    
-   experiment.PDF(y)
+   experiment.getPDF(y)
     
